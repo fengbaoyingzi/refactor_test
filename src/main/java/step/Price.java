8d02af89eaaf5d1c04ed6fcb67ba15cd14d35ef5
maxiaoqiang <xiaoqiang.ma@thoughtworks.com>
2019-09-09 20:52:28 +0800

    move getFrequentRenterPoints method from Movie class to Price class and replace conditional in getFrequentRenterPoints method with polymorphism

--- src/main/java/step/Price.java
+++ src/main/java/step/Price.java
@@ -4,4 +4,8 @@ abstract class Price {
     abstract int getPriceCode();
 
     abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented){
+        return 1;
+    }
 }

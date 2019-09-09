d93626805f8d8c89d9dd16e4529c4c405acf5950
maxiaoqiang <xiaoqiang.ma@thoughtworks.com>
2019-09-09 19:52:38 +0800

    add getCharge unit test

--- src/test/java/CustomerTest1.java
+++ src/test/java/CustomerTest1.java
@@ -7,7 +7,7 @@ import step.Rental;
 public class CustomerTest1 {
 
 	@Test
-	public void statement() {
+	public void testStatement() {
 		Customer customer = new Customer("xiaoqiang");
 		Movie movie = new Movie("哪吒", 2);
 		Rental rental = new Rental(movie, 7);
@@ -24,5 +24,44 @@ public class CustomerTest1 {
 		System.out.println(result);
 		System.out.println(expection);
 		Assert.assertEquals(result, expection);
+	}
+
+	@Test
+	public void testGetChargeForNewRelease() {
+		Movie movie = new Movie("Spider-Man", 1);
+		Rental rental = new Rental(movie, 3);
+		double result = rental.getCharge();
+		double expection = 9.0;
+		Assert.assertEquals(result, expection);
+	}
+
+	@Test
+	public void testGetChargeForChildren() {
+		Movie movie = new Movie("Big-Hero-6", 2);
+		Rental rental = new Rental(movie, 4);
+		double result = rental.getCharge();
+		double expection = 3.0;
+		Assert.assertEquals(result, expection);
+
+		Rental rental2 = new Rental(movie, 3);
+		double result2 = rental2.getCharge();
+		double expection2 = 1.5;
+		Assert.assertEquals(result2, expection2);
+	}
+
+	@Test
+	public void testGetChargeForRegular() {
+		Movie movie = new Movie("Iron-Man", 0);
+		Rental rental = new Rental(movie, 5);
+		double result = rental.getCharge();
+		double expection = 6.5;
+		Assert.assertEquals(result, expection);
+
+		Rental rental2 = new Rental(movie, 1);
+		double result2 = rental2.getCharge();
+		double expection2 = 2.0;
+		Assert.assertEquals(result2, expection2);
+
+
 	}
 }

601f97f71d616fa77da4a68739e6257b274cdf53
maxiaoqiang <xiaoqiang.ma@thoughtworks.com>
2019-09-09 19:34:40 +0800

    move getCharge method from Customer class to Rental class

--- src/main/java/step/Customer.java
+++ src/main/java/step/Customer.java
@@ -28,7 +28,7 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            thisAmount += getCharge(each);
+            thisAmount += each.getCharge();
             frequentRenterPoints++;
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
                     && each.getDaysRented() > 1)
@@ -44,23 +44,4 @@ public class Customer {
         return result;
     }
 
-    public double getCharge(Rental rental){
-        double result = 0;
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (rental.getDaysRented() > 2)
-                    result += (rental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (rental.getDaysRented() > 3)
-                    result += (rental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
 }
